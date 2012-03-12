75464fa110b22f72a2bfb36e364ab131fc8c4ef2
matto3c <matto3c@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-03-12 19:26:03 +0000

    changed to free version
    git-svn-id: https://plugins.svn.wordpress.org/extrawatch/trunk@518044 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- extrawatch.php
+++ extrawatch.php
@@ -42,21 +42,14 @@ if (@function_exists("extrawatch_admin_menu")) {
         add_menu_page(__('ExtraWatch', 'extrawatch'), __('ExtraWatch', 'extrawatch'), $EC_userLevel, 'extrawatch', 'jw_plugin_options', $path.'components/com_extrawatch/icons/extrawatch-logo-16x16.gif');
 
         add_submenu_page('extrawatch','Live Stats','Live Stats',$EC_userLevel,'extrawatch',array(&$this, 'extrawatch'));
-        add_submenu_page('extrawatch','SEO','SEO',$EC_userLevel,'extrawatch&task=seo',array(&$this, 'extrawatch&task=seo'));
-        add_submenu_page('extrawatch','Heatmap','Heatmap',$EC_userLevel,'extrawatch&task=heatmap',array(&$this, 'extrawatch&task=heatmap'));
-        add_submenu_page('extrawatch','Traffic Flow','Traffic Flow',$EC_userLevel,'extrawatch&task=flow',array(&$this, 'extrawatch&task=flow'));
         add_submenu_page('extrawatch','Graphs and Trends','Graphs and Trends',$EC_userLevel,'extrawatch&task=graphs',array(&$this, 'extrawatch&task=graphs'));
         add_submenu_page('extrawatch','Goals','Goals',$EC_userLevel,'extrawatch&task=goals',array(&$this, 'extrawatch&task=goals'));
         add_submenu_page('extrawatch','Visit History','Visit History',$EC_userLevel,'extrawatch&task=history',array(&$this, 'extrawatch&task=history'));
         add_submenu_page('extrawatch','Anti-spam and Blocking','Anti-spam and Blocking',$EC_userLevel,'extrawatch&task=antiSpam',array(&$this, 'extrawatch&task=antiSpam'));
         add_submenu_page('extrawatch','Email Reports','Email Reports',$EC_userLevel,'extrawatch&task=emails',array(&$this, 'extrawatch&task=emails'));
         add_submenu_page('extrawatch','Your License','Your License',$EC_userLevel,'extrawatch&task=license',array(&$this, 'extrawatch&task=license'));
-        add_submenu_page('extrawatch','Database Status','Database Status',$EC_userLevel,'extrawatch&task=status',array(&$this, 'extrawatch&task=status'));
-        add_submenu_page('extrawatch','Directory Sizes','Directory Sizes',$EC_userLevel,'extrawatch&task=sizes',array(&$this, 'extrawatch&task=sizes'));
         add_submenu_page('extrawatch','Settings','Settings',$EC_userLevel,'extrawatch&task=settings',array(&$this, 'extrawatch&task=settings'));
         add_submenu_page('extrawatch','Credits','Credits',$EC_userLevel,'extrawatch&task=credits',array(&$this, 'extrawatch&task=credits'));
-
-        //add_submenu_page( 'edit.php?post_type=theme', __('Theme Page Options'), __('Theme Menu Options'), 'edit_themes', 'theme_options', theme_options);
     }
 
     function jw_plugin_options()
