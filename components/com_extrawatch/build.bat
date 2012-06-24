d898e6b74b60a4362b3acf5ae4265fd39cdd1500
matto3c <matto3c@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-06-24 09:55:45 +0000

    build: deleted obsolete files after refactor
    git-svn-id: https://plugins.svn.wordpress.org/extrawatch/trunk@563109 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- components/com_extrawatch/build.bat
+++
@@ -1,5 +0,0 @@
-set PATH=%PATH%;g:\prog\ant\bin;d:\prog\svn\bin\
-
-set ANT_HOME=d:\prog\ant
-
-d:\prog\ant\bin\ant %*
--- components/com_extrawatch/create_symlinks.bat
+++
@@ -1,2 +0,0 @@
-junction d:\work\cg15\components\com_extrawatch  d:\work\extrawatch\components\com_extrawatch
-junction d:\work\cg15\administrator\components\com_extrawatch  d:\work\extrawatch\administrator\components\com_extrawatch
