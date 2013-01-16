451dc76b3060f6b3b308343250dc09ffedad982a
matto3c <matto3c@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-01-16 21:39:00 +0000

    updated to revision 469
    git-svn-id: https://plugins.svn.wordpress.org/extrawatch/trunk@653895 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- components/com_extrawatch/src/env/wordpress/extrawatch.php
+++ components/com_extrawatch/src/env/wordpress/extrawatch.php
@@ -1,13 +1,13 @@
 <?php
 /*
-Plugin Name: ExtraWatch FREE
+Plugin Name: ExtraWatch Live Stats and Visitor Counter FREE
 Plugin URI: http://www.codegravity.com
 Description: Popular visitor live tracking component ported from Joomla. <a href="http://youtu.be/w8-LITejoV0?hd=1">View demo video</a>.
-Demo server: <a href="http://codegravitydemo.com/wordpress/">Frontpage</a> / <a href="http://codegravitydemo.com/wordpress/wp-admin/">Admin</a>.
+Demo server: <a href="http://codegravitydemo.com/wordpress/FREE/">Frontpage</a> / <a href="http://codegravitydemo.com/wordpress/FREE/wp-admin/">Admin</a>.
 Features: <strong>Visitor Live Stats</strong>, <strong>History</strong>,
 <strong>Graphs</strong>, <strong>Anti-spam</strong>, <strong>Nightly Email Reports</strong>,
 <strong>Front-end Counters</strong>, translated in <strong>42 world languages</strong>
-Version: 1.2.18.465 FREE
+Version: 1.2.18.469 FREE
 Author: Matej Koval
 Author URI: http://www.codegravity.com
 */
