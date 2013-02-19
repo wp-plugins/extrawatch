bff345986058762a019daff447bd8d565714ef33
matto3c <matto3c@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-02-19 19:46:57 +0000

    updated to revision bugfix: broken logo fix - by adding ob_end_clean();
    git-svn-id: https://plugins.svn.wordpress.org/extrawatch/trunk@670467 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- components/com_extrawatch/view/license-free.php
+++ components/com_extrawatch/view/license-free.php
@@ -51,7 +51,7 @@ function extrawatch_renderLicenseFree($extraWatch)
         font-weight: normal;
     }
 </style>
-<h1>ExtraWatch 1.2.18.513 FREE</h1>
+<h1>ExtraWatch 1.2.18.514 FREE</h1>
 
 <script type='text/javascript'>
     function toggleNoKeyForm() {
