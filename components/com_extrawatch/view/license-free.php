da6e29979d5e815185af70eff7c0a778da957012
matto3c <matto3c@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-06-19 06:10:34 +0000

    social media: new feature to detect visits from facebook, twitter, linkedin, google+, myspace, etc.
    git-svn-id: https://plugins.svn.wordpress.org/extrawatch/trunk@728218 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- components/com_extrawatch/view/license-free.php
+++ components/com_extrawatch/view/license-free.php
@@ -51,7 +51,7 @@ function extrawatch_renderLicenseFree($extraWatch)
         font-weight: normal;
     }
 </style>
-<h1>ExtraWatch 2.1.761 FREE</h1>
+<h1>ExtraWatch 2.1.763 FREE</h1>
 
 <script type='text/javascript'>
     function toggleNoKeyForm() {
