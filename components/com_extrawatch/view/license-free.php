4c5aeea030b639d3553cbccd9255a2e001adcc1d
matto3c <matto3c@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-06-27 16:10:46 +0000

    mobile devices: are now identified in right stats section
    git-svn-id: https://plugins.svn.wordpress.org/extrawatch/trunk@732943 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- components/com_extrawatch/view/license-free.php
+++ components/com_extrawatch/view/license-free.php
@@ -51,7 +51,7 @@ function extrawatch_renderLicenseFree($extraWatch)
         font-weight: normal;
     }
 </style>
-<h1>ExtraWatch 2.1.786 FREE</h1>
+<h1>ExtraWatch 2.1.787 FREE</h1>
 
 <script type='text/javascript'>
     function toggleNoKeyForm() {
