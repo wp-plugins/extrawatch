2655ac69cea74cbcad189122fc0f1fe4783cab7f
matto3c <matto3c@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-10-11 20:50:00 +0000

    updated readme.txt
    git-svn-id: https://plugins.svn.wordpress.org/extrawatch/trunk@786623 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- components/com_extrawatch/view/license-free.php
+++ components/com_extrawatch/view/license-free.php
@@ -51,7 +51,7 @@ function extrawatch_renderLicenseFree($extraWatch)
         font-weight: normal;
     }
 </style>
-<h1>ExtraWatch 2.2.1211 FREE</h1>
+<h1>ExtraWatch 2.2.1212 FREE</h1>
 
 <script type='text/javascript'>
     function toggleNoKeyForm() {
