ec7c5f545603a4e5cff0cd421363d0e2f86c9eeb
matto3c <matto3c@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-06-27 19:53:18 +0000

    svn: E205000: Try 'svn help propget' for more information svn: E205000: Wrong number of targets specified
    git-svn-id: https://plugins.svn.wordpress.org/extrawatch/trunk@939829 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- components/com_extrawatch/view/sizes.php
+++ components/com_extrawatch/view/sizes.php
@@ -5,7 +5,7 @@
  * ExtraWatch - A real-time ajax monitor and live stats  	 	    	    		  	 	  	 	  		 	 		    	 			 	   		  	 	 		 	 	   	      	  	 		 		 				 			 		  		    	 		 		  
  * @package ExtraWatch  	 	    	    		  	 	  	 	  		 	 		    	 			 	   		  	 	 		 	 	   	      	  	 		 		 				 			 		  		    	 		 		  
  * @version 2.3  	 	    	    		  	 	  	 	  		 	 		    	 			 	   		  	 	 		 	 	   	      	  	 		 		 				 			 		  		    	 		 		  
- * @revision 2021  	 	    	    		  	 	  	 	  		 	 		    	 			 	   		  	 	 		 	 	   	      	  	 		 		 				 			 		  		    	 		 		  
+ * @revision 2026  	 	    	    		  	 	  	 	  		 	 		    	 			 	   		  	 	 		 	 	   	      	  	 		 		 				 			 		  		    	 		 		  
  * @license http://www.gnu.org/licenses/gpl-3.0.txt     GNU General Public License v3  	 	    	    		  	 	  	 	  		 	 		    	 			 	   		  	 	 		 	 	   	      	  	 		 		 				 			 		  		    	 		 		  
  * @copyright (C) 2014 by CodeGravity.com - All rights reserved!  	 	    	    		  	 	  	 	  		 	 		    	 			 	   		  	 	 		 	 	   	      	  	 		 		 				 			 		  		    	 		 		  
  * @website http://www.codegravity.com  	 	    	    		  	 	  	 	  		 	 		    	 			 	   		  	 	 		 	 	   	      	  	 		 		 				 			 		  		    	 		 		  
@@ -13,7 +13,7 @@
 defined('_JEXEC') or die('Restricted access'); ?>
 
 
-<h1><?php echo _EW_ADMINHEADER_HEATMAP;?></h1>
+<h1><?php echo _EW_ADMINHEADER_COMPONENTS;?></h1>
 <h2><?php echo _EW_MENU_AVAILABLE_IN_PRO_COMPONENTS;?></h2>
 <?php echo ($extraWatchHTML->renderAvailableInFullVersion("sizes"));?>
 
